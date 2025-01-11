5c3f53721841e136d0fa530cea90b0d0d3eccbfd
Cristian Gomez <cg231193@gmail.com>
2025-01-10 23:12:03 -0500

    Laboratorio 8

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Pokemon, Trainer
+
+
 
 # Register your models here.
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    pass 
+
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'level', 'birth_date')
--- pokedex/urls.py
+++ pokedex/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:pokemon>/", views.pokemon, name="pokemon"),
-]
+    path("<int:pokemon_id>/", views.pokemon, name="pokemon"),
+    ]
